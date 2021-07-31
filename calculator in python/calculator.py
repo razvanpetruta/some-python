2cd5aa19de814b201238f183b6740741144fec3b
razvanpetruta <82545346+razvanpetruta@users.noreply.github.com>
2021-07-31 19:12:47 +0300

    Add files via upload

---
+++ calculator in python/calculator.py
@@ -0,0 +1,104 @@
+from tkinter import *
+
+root = Tk()
+root.title("Calculator")
+
+e = Entry(root, width=40, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+# functions
+
+def button_click(number):
+   current = e.get() 
+   e.delete(0, END) 
+   e.insert(0, str(current) + str(number))
+
+def buttonClear():
+   e.delete(0, END)
+
+def add_numbers():
+   global first_number 
+   global math
+   math = "addition" 
+   first_number = int(e.get())
+   e.delete(0, END)
+
+def substract_numbers():
+   global first_number 
+   global math
+   math = "subtraction" 
+   first_number = int(e.get())
+   e.delete(0, END)
+
+def multiply_numbers():
+   global first_number 
+   global math
+   math = "multiplication" 
+   first_number = int(e.get())
+   e.delete(0, END)
+
+def divide_numbers():
+   global first_number 
+   global math
+   math = "division" 
+   first_number = int(e.get())
+   e.delete(0, END)
+
+def buttonEqual():
+   second_number = int(e.get())
+   e.delete(0, END)
+
+   if math == "addition":
+      e.insert(0, first_number + second_number)
+   elif math == "subtraction":
+      e.insert(0, first_number - second_number)
+   elif math == "multiplication":
+      e.insert(0, first_number * second_number)
+   else:
+      e.insert(0, first_number / second_number)
+
+# create buttons
+
+button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0), bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+
+button_add = Button(root, text="+", padx=39, pady=20, command=add_numbers, bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_equal = Button(root, text="   =  ", padx=79, pady=20, command=buttonEqual, bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_clear = Button(root, text="  CE ", padx=79, pady=20, command=buttonClear, bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+
+button_subtract = Button(root, text="- ", padx=39, pady=20, command=substract_numbers, bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_multiply = Button(root, text="*", padx=40, pady=20, command=multiply_numbers, bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+button_divide = Button(root, text="/ ", padx=39, pady=20, command=divide_numbers, bg="#7FD8BE", fg="#3C3744", activebackground="#CFCBCA")
+
+# put the buttons on the screen
+
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+
+root.mainloop()
